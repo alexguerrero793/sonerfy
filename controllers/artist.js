eece921f519d318eb5c3553cf8ab31c8860cfee6
alexguerrero793 <alexguerrero793@gmail.com>
2018-08-21 10:37:59 +0200

    creacion-master

--- controllers/artist.js
+++ controllers/artist.js
@@ -9,9 +9,32 @@ var Song = require('../models/song');
 
 function getArtist(req, res){
     res.status(200).send({message: 'mdndnd'});
-
 }
 
+
+function saveArtist(req, res){
+    var artist = new Artist();
+    var params = req.body;
+    artist.name = params.name;
+    artist.description = params.description;
+    artist.image = 'null';
+
+    artist.sabe((er, artistStores) => {
+        if(err){
+            res.status(500).send({message: 'Error al guardar el artista'});
+        }else{
+            if(!artistStored){
+                res.status(404).send({message: 'Error al guardar el artista'});
+            }else{
+                res.status(200).send({artist: artistStored});
+            }
+        }
+    });
+}
+
+
+
 module.exports = {
-    getArtist
+    getArtist,
+    saveArtist
 };
--- models/album.js
+++ models/album.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var AlbumSchema = Schema({
     title: String,
     description: String,
-    year: number,
+    year: String,
     image: String,
     artist: { type: Schema.ObjectId, ref: 'Artist'}
 
